a2d7e4ae320abb73914776d32efe2d3b6f8d4fe7
VaishnaviMahajan1211 <mahajankumkum789@gmail.com>
2026-07-17 18:22:37 +0530

    Added database schema script

---
+++ sql/02_master_tables.sql
@@ -0,0 +1,45 @@
+
+
+USE skyflow_db;
+
+CREATE TABLE Airport (
+    airport_id INT AUTO_INCREMENT PRIMARY KEY,
+    airport_code CHAR(3) NOT NULL UNIQUE,
+    airport_name VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(100),
+    country VARCHAR(100) NOT NULL,
+    timezone VARCHAR(50),
+    latitude DECIMAL(9,6),
+    longitude DECIMAL(9,6),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+select* from Airport; 
+
+
+CREATE TABLE Aircraft (
+    aircraft_id INT AUTO_INCREMENT PRIMARY KEY,
+    aircraft_code VARCHAR(20) NOT NULL UNIQUE,
+    manufacturer VARCHAR(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    capacity INT NOT NULL,
+    manufacturing_year YEAR,
+    airline_name VARCHAR(100),
+    status ENUM('Active','Maintenance','Retired') DEFAULT 'Active',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Passenger (
+    passenger_id INT AUTO_INCREMENT PRIMARY KEY,
+    passenger_code VARCHAR(20) NOT NULL UNIQUE,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    gender ENUM('Male','Female','Other'),
+    date_of_birth DATE,
+    email VARCHAR(100) UNIQUE,
+    phone_number VARCHAR(20),
+    passport_number VARCHAR(30) UNIQUE,
+    nationality VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
